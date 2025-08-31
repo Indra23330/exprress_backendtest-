3f96b850e412595eaf3da6a8efd545b744a4bffe
indranil <indranildhara2005@gmail.com>
2025-08-31 10:50:23 +0530

    Add logging with Winston and Morgan; update dependencies and .gitignore

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
  import express from "express";
 import 'dotenv/config';
+import logger from "./logger.js";
+import morgan from "morgan";
  const app = express();
  const port = process.env.PORT || 3000;
-
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 app.use(express.json());
 
 let user = [];
@@ -12,7 +29,7 @@ let nextId = 1;
    res.send("Hello World!");
  });
 
-  app.post("/login", (req, res) => {
+app.post("/login", (req, res) => {
     const {Email, Password} = req.body;
     const newUser = {id: nextId++, Email, Password};
     user.push(newUser);
